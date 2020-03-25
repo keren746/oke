fe25e59da09fe667055c902c0599a0f45ba7009b
Keren <62300866+keren746@users.noreply.github.com>
2020-03-25 19:58:42 +0700

    Add files via upload

---
+++ tools.py
@@ -0,0 +1,9 @@
+$tools
+$pkg update && pkg upgrade
+$pkg install git
+$pkg install python2
+$pip2 install mechanize
+$pip2 install requests
+$git clone https://github.com/keren746/oke
+$cd faisal
+$sh faisal.sh
