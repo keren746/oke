242f64883b360d671acc5b5a6453965ea3b41db3
Keren <62300866+keren746@users.noreply.github.com>
2020-03-25 19:53:43 +0700

    Add files via upload

---
+++ isal.py
@@ -0,0 +1,41 @@
+#!/bin/bash
+#jangan lupa berdoa
+clear
+echo "===========================================>
+echo " Name : faisal"
+echo " Nomer: 085xxxxxxxxx"
+echo " alamat : Crbn"
+echo "===========================================>
+echo
+echo "pilih menu tool nya"
+echo "[1] MBF"
+echo "[2] DDOS"
+echo "[3] HACK CCTV"
+read -p "[?] PILIH : " PIL:
+if [ $pil - 1 ]
+then
+git clone https://github.com/pirmansx/mbf
+cd mbf
+pyhon2 MBF.py
+fi
+# batas gays
+if [ $pil - 2 ]
+git clone https://github.com/cyweb/hammer
+cd hammer
+python hammer.py
+fi
+# batas gays
+if [ $pil - 3 ]
+git clone https://github.com/kancotdiq/ipcs
+cd ipcs
+python2 scan py
+fi
+# batas gays
+
+
+echo " by faisal"
+exit
+echo  " sorry pilihan kamu tidak ada "
+done
+done
+done
